dc50fb991d30e1c2f021cc892a7a1ca0ae52c616
Hoang Tran Thanh Dat <dathoang31082001@gmail.com>
2025-05-14 10:38:51 +0700

    [ADD] odoo_web_service: implement XML-RPC connection for employee data retrieval
    This commit introduces a new script, `odoo_web_service.py`, which establishes a connection to the Odoo server using XML-RPC. It retrieves the server version and authenticates the user, then queries the `hr.employee` model to fetch employee IDs and their details, including names and job titles.
    
    Additionally, the `start_odoo.txt` file has been updated to include instructions for restarting the Odoo server with the custom API.
    
    closes odoo/odoo#XXXXXX (replace with actual issue number if applicable)

---
+++ odoo_web_service.py
@@ -0,0 +1,36 @@
+import xmlrpc.client
+
+# Thông tin kết nối
+url = 'http://localhost:8070'
+db = 'LansingApp'
+username = 'dathoang31082001@gmail.com'
+password = '31082001Dat!'
+
+# Bước 1: Kết nối với endpoint /xmlrpc/2/common để lấy thông tin version và xác thực
+common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
+
+# Lấy thông tin version của Odoo
+version = common.version()
+print("Odoo Server Details: ", version)
+
+# Xác thực để lấy UID
+uid = common.authenticate(db, username, password, {})
+if uid:
+    print("Authentication successful, UID: ", uid)
+else:
+    print("Authentication failed. Check your username, password, or database name.")
+    exit()
+
+# Bước 2: Kết nối với endpoint /xmlrpc/2/object để truy vấn dữ liệu
+models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(url))
+
+# Tìm kiếm danh sách nhân viên (hr.employee)
+employee_ids = models.execute_kw(db, uid, password, 'hr.employee', 'search', [[]])
+print("Employee IDs: ", employee_ids)
+
+# Lấy thông tin chi tiết của nhân viên (tên, chức vụ)
+if employee_ids:
+    employees = models.execute_kw(db, uid, password, 'hr.employee', 'read', [employee_ids], {'fields': ['name', 'job_title']})
+    print("Employees: ", employees)
+else:
+    print("No employees found.")
